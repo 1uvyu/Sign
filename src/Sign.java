37b767a62982c73ff79c380bcc7cd60a0b73f098
KayleeChan <kayleec55@nycstudents.net>
2025-12-19 12:23:30 -0500

    Added test cases and wrote the numberOfLines method.

--- src/Sign.java
+++ src/Sign.java
@@ -1,7 +1,30 @@
 public class Sign
 {
+    private String message;
+    private int width;
+
     public Sign(String message, int width)
     {
-        
+        this.message = message;
+        this.width = width;
+    }
+
+    public int numberOfLines()
+    {
+        String str = message;
+        int count = 0;
+        while (str.length() > width)
+        {
+            str = str.substring(width);
+            count++;
+        }
+        if (str.length() > 0) count ++;
+        return count;
+    }
+
+    public String getLines()
+    {
+        return null;
     }
 }
+// message.substring(0, width);
